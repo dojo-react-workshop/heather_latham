261f008b7bcd3592352dc115405bd6b1064fe6ca
Heather Latham <heatherlatham@northwesternmutual.com>
2017-05-25 09:07:31 -0500

    updated stock gains

--- coding_challenges/stock-gains.js
+++ coding_challenges/stock-gains.js
@@ -4,55 +4,52 @@
 //return best profit could have made from one purchase and one sale
 //for this one, buy low at 5 and sell high at 11
 
-const applePricesYesterday = [5,4,3,2,1,1]; //different trades at these prices
+// function maxProfit(arr) {
+//     var buyIndex = 0;
+//     var sellIndex = 1;
+//     var maxProfit = arr[sellIndex] - arr[buyIndex];
 
-
-function maxProfit(arr) {
-    var buyIndex = 0;
-    var sellIndex = 1;
-    var maxProfit = arr[sellIndex] - arr[buyIndex];
-
-   for(let i=0; i<arr.length; i++) {
-        for(let x=i+1; x<arr.length; x++) {
+//    for(let i=0; i<arr.length; i++) {
+//         for(let x=i+1; x<arr.length; x++) {
           
-            if((arr[x]-arr[i]) > maxProfit) {
-                buyIndex = i;
-                sellIndex = x;
-                maxProfit = arr[x]-arr[i];
-            }
-        }
-    }
+//             if((arr[x]-arr[i]) > maxProfit) {
+//                 buyIndex = i;
+//                 sellIndex = x;
+//                 maxProfit = arr[x]-arr[i];
+//             }
+//         }
+//     }
 
-   return({buyIndex:buyIndex, sellIndex:sellIndex, maxProfit:maxProfit});
-}
-
-console.log(maxProfit(applePricesYesterday)); 
-
-
-// function getMaxProfit(applePricesYesterday, ind){
-// 	if (ind+1 >= applePricesYesterday.length){
-// 		// throw new Error('Must provide more than 1 price');
-// 		return;
-// 	}
-
-// 	let maxDiff = applePricesYesterday[ind+1] - applePricesYesterday[ind];
-
-// 	for (let i = ind+2; i < applePricesYesterday.length; i++){
-// 		let diff = applePricesYesterday[i] - applePricesYesterday[ind];
-// 		if (diff > maxDiff){
-// 			maxDiff = diff;
-// 		}
-// 	}
-
-// 	let nextMax = getMaxProfit(applePricesYesterday, ind+1);
-// 	if (nextMax === undefined){
-// 		return maxDiff;
-// 	} else if (maxDiff > nextMax){
-// 		return maxDiff;
-// 	} else {
-// 		return nextMax;
-// 	}
-
+//    return({buyIndex:buyIndex, sellIndex:sellIndex, maxProfit:maxProfit});
 // }
 
-// console.log(getMaxProfit(applePricesYesterday, 0)); 
+// console.log(maxProfit(applePricesYesterday)); 
+
+//use this one to only loop once (this is hte concept of the greedy solution, means 
+//keeping the best solution so far and throwing away the rest)
+
+function maxProfit(arr) {
+    if(arr.length < 2){
+        throw new Error('Must provide at least 2 prices.');
+    }
+    let buy = arr[0];
+    let maxProfit = arr[1] - arr[0];
+
+   for(let i=1; i<arr.length; i++) {
+        if((arr[i]-buy) > maxProfit){
+            maxProfit = arr[i]-buy;
+        }
+        if(arr[i] < buy){
+            buy = arr[i];
+        } 
+    }
+   return maxProfit;
+}
+
+var arr=[2,3,5,7,13,9]
+const applePricesYesterday = [10,7,5,8,11,9]
+var arr_2 = [1,-3]
+
+console.log(maxProfit(arr))
+console.log(maxProfit(arr_2))
+console.log(maxProfit(applePricesYesterday))
